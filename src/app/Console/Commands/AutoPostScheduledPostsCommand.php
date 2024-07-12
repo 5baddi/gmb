d7de8681933a4b433f97e88b634c03b446f7d764
5baddi <project@baddi.info>
2024-07-12 18:56:01 +0100

    psr

--- src/app/Console/Commands/AutoPostScheduledPostsCommand.php
+++ src/app/Console/Commands/AutoPostScheduledPostsCommand.php
@@ -52,7 +52,7 @@ class AutoPostScheduledPostsCommand extends Command
 
         try {
             ScheduledPost::query()
-                ->where(ScheduledPost::SCHEDULED_AT_COLUMN, "<=", Carbon::now()->format('Y-m-d H:i:s'))
+                ->where(ScheduledPost::SCHEDULED_AT_COLUMN, '<=', Carbon::now()->format('Y-m-d H:i:s'))
                 ->where(ScheduledPost::STATE_COLUMN, '!=', ScheduledPost::REJECTED_STATE)
                 ->chunkById(10, function (Collection $scheduledPosts) {
                     $scheduledPosts->each(function (ScheduledPost $scheduledPost) {
