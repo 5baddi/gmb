997576f8de83704fb2538f5f6941077facda66bf
5baddi <project@baddi.info>
2024-07-27 00:06:36 +0100

    wip: reject reason

--- src/app/Domains/GoogleMyBusinessService.php
+++ src/app/Domains/GoogleMyBusinessService.php
@@ -324,7 +324,7 @@ class GoogleMyBusinessService extends Service
                 return Str::endsWith($key, '.message');
             }, ARRAY_FILTER_USE_KEY);
 
-            throw new Exception(implode(PHP_EOL, $messages));
+            throw new Exception(implode(' ', $messages));
         }
 
         return $response->getStatusCode() === 200;
