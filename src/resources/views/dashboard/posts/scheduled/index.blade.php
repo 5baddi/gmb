fa82d8132e9d8bb19a26c9a2ad48fdc73f5d2760
5baddi <project@baddi.info>
2024-08-06 08:57:23 +0100

    hotfix: list scheduled posts

--- src/resources/views/dashboard/posts/scheduled/index.blade.php
+++ src/resources/views/dashboard/posts/scheduled/index.blade.php
@@ -1,4 +1,7 @@
-@php use Illuminate\Support\Str; @endphp
+@php
+    use Illuminate\Support\Str;
+    use BADDIServices\ClnkGO\Models\ScheduledPost;
+@endphp
 
 @extends('layouts.dashboard')
 
@@ -130,7 +133,7 @@
                                                 </a>
                                             @endif
                                             @if(in_array(Str::lower($scheduledPost->state), ['unspecified', 'rejected']))
-                                                <a href="{{ route('dashboard.scheduled-posts.edit', ['type' => Str::lower($scheduledPost->topic_type), 'id' => $scheduledPost->id]) }}" class="btn btn-sm btn-default" title="{{ trans('global.edit') }}">
+                                                <a href="{{ route('dashboard.scheduled-posts.edit', ['type' => Str::lower($scheduledPost->topic_type ?? ScheduledPost::STANDARD_TYPE), 'id' => $scheduledPost->id]) }}" class="btn btn-sm btn-default" title="{{ trans('global.edit') }}">
                                                     <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg>
                                                 </a>
                                             @endif
