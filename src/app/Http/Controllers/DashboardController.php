6488c1275987b349e9f298b3c6ee0ae1c5f3f700
5baddi <project@baddi.info>
2024-09-11 12:25:45 +0100

    feat: cron to refresh users google access tokens

--- src/app/Http/Controllers/DashboardController.php
+++ src/app/Http/Controllers/DashboardController.php
@@ -9,21 +9,17 @@
 namespace BADDIServices\ClnkGO\Http\Controllers;
 
 use App\Models\User;
-use App\Jobs\PullAccountLocations;
 use Illuminate\Contracts\View\View;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Contracts\View\Factory;
-use BADDIServices\ClnkGO\Models\Pack;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use BADDIServices\ClnkGO\Services\UserService;
 use BADDIServices\ClnkGO\Domains\GoogleService;
 use BADDIServices\ClnkGO\Models\AccountLocation;
 use Illuminate\Routing\Controller as BaseController;
-use BADDIServices\ClnkGO\Models\UserGoogleCredentials;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use BADDIServices\ClnkGO\Domains\GoogleMyBusinessService;
-use BADDIServices\ClnkGO\Models\ObjectValues\GoogleCredentialsObjectValue;
 
 class DashboardController extends BaseController
 {
@@ -45,8 +41,6 @@ class DashboardController extends BaseController
             $this->user = Auth::id() !== null ? $this->userService->findById(Auth::id()) : null;
 
             $this->googleService = app(GoogleService::class);
-            $this->googleService->refreshAccessToken($this->user->googleCredentials);
-            $this->user->load(['googleCredentials']); // reload user google credentials
 
             $this->googleMyBusinessService = new GoogleMyBusinessService(
                 $this->user->googleCredentials?->getAccessToken(),
@@ -69,14 +63,6 @@ class DashboardController extends BaseController
             ->where(AccountLocation::USER_ID_COLUMN, $this->user->getId())
             ->get()
             ->toArray();
-
-        // Fixme: remove this auto pull
-        if (sizeof($accountLocations) === 0 && $this->user->googleCredentials instanceof UserGoogleCredentials) {
-            PullAccountLocations::dispatch(
-                $this->user->getId(),
-                GoogleCredentialsObjectValue::fromArray($this->user->googleCredentials->toArray())
-            );
-        }
 
         return [
             'user'                  => $this->user,
