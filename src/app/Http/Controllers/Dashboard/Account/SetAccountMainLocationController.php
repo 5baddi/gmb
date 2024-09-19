3a8230052f2de94733123dbe552ebefae100dc80
5baddi <project@baddi.info>
2024-09-19 17:34:16 +0100

    improve: set main location

--- src/app/Http/Controllers/Dashboard/Account/SetAccountMainLocationController.php
+++ src/app/Http/Controllers/Dashboard/Account/SetAccountMainLocationController.php
@@ -11,8 +11,10 @@ namespace BADDIServices\ClnkGO\Http\Controllers\Dashboard\Account;
 use Throwable;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Http\RedirectResponse;
 use BADDIServices\ClnkGO\Entities\Alert;
+use BADDIServices\ClnkGO\Models\AccountLocation;
 use BADDIServices\ClnkGO\Models\UserGoogleCredentials;
 use BADDIServices\ClnkGO\Http\Controllers\DashboardController;
 use BADDIServices\ClnkGO\Models\ObjectValues\GoogleCredentialsObjectValue;
@@ -22,14 +24,27 @@ class SetAccountMainLocationController extends DashboardController
     public function __invoke(Request $request): RedirectResponse
     {
         try {
+            $locationId = Str::replace('locations/', '', $request->get('name'));
+            abort_if(empty($locationId), Response::HTTP_UNPROCESSABLE_ENTITY);
+
+            /** @var AccountLocation $accountLocation */
+            $accountLocation = AccountLocation::query()
+                ->where(AccountLocation::USER_ID_COLUMN, $this->user->getId())
+                ->where(AccountLocation::LOCATION_ID_COLUMN, $locationId)
+                ->first();
+
+            abort_unless($accountLocation instanceof AccountLocation, Response::HTTP_NOT_FOUND);
+
             $this->userService->saveGoogleCredentials(
                 $this->user,
                 GoogleCredentialsObjectValue::fromArray(
                     array_merge(
                         $this->user->googleCredentials->toArray(),
                         [
+                            UserGoogleCredentials::ACCOUNT_ID_COLUMN
+                            => $accountLocation->getAccountId(),
                             UserGoogleCredentials::MAIN_LOCATION_ID_COLUMN
-                            => Str::replace('locations/', '', $request->get('name'))
+                            => $accountLocation->getLocationId(),
                         ]
                     )
                 )
